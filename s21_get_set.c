4788ee69d49e362a1691ae2cb9380abda2482a94
vasiliev arsen <arsen3393@gmail.com>
2024-03-20 13:42:50 +0900

    2 commit

--- s21_get_set.c
+++ s21_get_set.c
@@ -12,8 +12,11 @@ int s21_get_bit(s21_decimal number, int index){
 // установка бита по индексу
 void s21_set_bit(s21_decimal *number, int index, int bit){
     int mask = 1u << (index%32);
+    if (bit == 0)
+        number -> bits[index/32] = number -> bits[index/32] & ~mask;
+    else
+        number -> bits[index/32] = number -> bits[index/32] | mask;
 }
-
 
 
 // получить знак возвращает 1 если положительное, 0 если отрицательное
@@ -22,7 +25,7 @@ int s21_get_sign(s21_decimal number){
 }
 
 // установка знака
-
+//////
 
 // обнуление децимал
 
@@ -31,3 +34,5 @@ void s21_zero_decimal(s21_decimal *number){
         number->bits[i] = 0;
     }
 }
+
+
---
+++ unit_tests/s21_decimal_test.h
@@ -0,0 +1,14 @@
+#ifndef S21_DECIMAL_TEST_H
+#define S21_DECIMAL_TEST_H
+
+
+#include <check.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define TEST_ARITHMETIC_OK 0
+#define TEST_ARITHMETIC_BIG
+#define TEST_ARITHMETIC_SMALL 2
+#define TEST_ARITHMETIC_ZERO_DIV 3
+
+#endif //S21_DECIMAL_TEST_H
